a4b9079c31f9e1a52c5133155de00b963862de4f
kavyadiju <70419997+kavyadiju@users.noreply.github.com>
2021-11-23 13:56:23 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -20,56 +20,10 @@ function setup(){
   tower.addImage("tower",towerImg);
   tower.velocityY = 1;
   
-  doorsGroup = new Group();
-  climbersGroup = new Group();
-  invisibleBlockGroup = new Group();
-  
-  ghost = createSprite(200,200,50,50);
-  ghost.scale = 0.3;
-  ghost.addImage("ghost", ghostImg);
 }
 
 function draw(){
   background(0);
-  if (gameState === "play") {
-    if(keyDown("left_arrow")){
-      ghost.x = ghost.x - 3;
-    }
-    
-    if(keyDown("right_arrow")){
-      ghost.x = ghost.x + 3;
-    }
-    
-    if(keyDown("space")){
-      ghost.velocityY = -10;
-    }
-    
-    ghost.velocityY = ghost.velocityY + 0.8
-    
-    if(tower.y > 400){
-      tower.y = 300
-    }
-    spawnDoors();
-
-    
-    //climbersGroup.collide(ghost);
-    if(climbersGroup.isTouching(ghost)){
-      ghost.velocityY = 0;
-    }
-    if(invisibleBlockGroup.isTouching(ghost) || ghost.y > 600){
-      ghost.destroy();
-      gameState = "end"
-    }
-    
-    drawSprites();
-  }
-  
-  if (gameState === "end"){
-    stroke("yellow");
-    fill("yellow");
-    textSize(30);
-    text("Game Over", 230,250)
-  }
 
 }
 
