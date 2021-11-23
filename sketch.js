c466ad34c853688dd14158ef719e5ca7b1dddae0
kavyadiju <70419997+kavyadiju@users.noreply.github.com>
2021-11-23 14:01:22 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -30,37 +30,7 @@ function draw(){
 function spawnDoors() {
   //write code here to spawn the doors in the tower
   if (frameCount % 240 === 0) {
-    var door = createSprite(200, -50);
-    var climber = createSprite(200,10);
-    var invisibleBlock = createSprite(200,15);
-    invisibleBlock.width = climber.width;
-    invisibleBlock.height = 2;
-    
-    door.x = Math.round(random(120,400));
-    climber.x = door.x;
-    invisibleBlock.x = door.x;
-    
-    door.addImage(doorImg);
-    climber.addImage(climberImg);
-    
-    door.velocityY = 1;
-    climber.velocityY = 1;
-    invisibleBlock.velocityY = 1;
-    
-    ghost.depth = door.depth;
-    ghost.depth +=1;
-   
-    //assign lifetime to the variable
-    door.lifetime = 800;
-    climber.lifetime = 800;
-    invisibleBlock.lifetime = 800;
-
-    
-    //add each door to the group
-    doorsGroup.add(door);
-    invisibleBlock.debug = true;
-    climbersGroup.add(climber);
-    invisibleBlockGroup.add(invisibleBlock);
+ 
   }
 }
 
